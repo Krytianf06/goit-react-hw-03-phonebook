bdff09d1b2da95a6040a6aed040804d5b4944260
Krystian Florkowski <86536950+Krytianf06@users.noreply.github.com>
2022-07-23 20:09:37 +0200

    Koniec 10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,18 +8,18 @@ import capitalize from 'utils/capitalize';
 
 const LOCAL_STORAGE_KEY = 'contacts';
 const INITIAL_STATE = {
-  contacts: [],
+  contacts: [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '129-21-23'},
+            ],
   filter: '',
 };
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '129-21-23' },
-    ],
+    contacts: [],
     filter: '',
   };
 
